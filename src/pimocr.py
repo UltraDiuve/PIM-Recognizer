f2dd9e20e29627a3e6b6bbe7d81182d46051d115
MASSE Pierre <pit.masse@gmail.com>
2020-01-06 14:19:55 +0100

    first try of class hierarchy

--- src/pimocr.py
+++ src/pimocr.py
@@ -12,8 +12,12 @@ class BaseOCR(object):
     This class defines the basic model of OCR subclasses and should not be
     instanciated.
     """
-    def __init__(self):
-        pass
+    def __init__(self, wrapper=None, tool_name=None):
+        self.wrapper = wrapper
+        self.tool_name = tool_name
+
+    def __str__(self):
+        print(':'.join(self.wrapper, self.tool_name))
 
     def set_file(self):
         pass
@@ -34,18 +38,19 @@ class PyorcWrappedOCR(BaseOCR):
     This class defines some functions for OCR tools based on pyocr library, and
     should not be instanciated.
     """
-    def __init__(self, tool_name=None):
+    def __init__(self, tool_name=None, **kwargs):
         """Constructor method for pyocr wrapped tools.
 
         tool_name arg should always be provided, and from the following list
         (see pyocr documentation): 'Tesseract (sh)', 'Tesseract (C-API)' or
         'Cuneiform (sh)'.
         """
+        if tool_name is None:
+            raise ValueError("Please specify a tool.")
         tool_list = map(lambda x: x.get_name(), pyocr.get_available_tools())
         if tool_name not in tool_list:
             raise ValueError(f"Tool {tool_name} not installed on current env.")
-        self.tool_name = tool_name
-        self.wrapper_type = 'pyocr'
+        super().__init__(tool_name=tool_name, wrapper='pyocr')
 
     def set_file(self):
         pass
@@ -87,8 +92,9 @@ class PyocrTextOCR(PyorcWrappedOCR, TextOCR):
 
     This class instanciates the pyocr raw text functionnality.
     """
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        self.builder = pyocr.builders.TextBuilder()
+        super().__init__(**kwargs)
 
     def set_file(self):
         pass
