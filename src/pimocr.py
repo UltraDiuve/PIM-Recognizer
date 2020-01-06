432b70850cb7498e2aa747155d75f08420e09540
MASSE Pierre <pit.masse@gmail.com>
2020-01-06 13:01:22 +0100

    initial commit of src folder

---
+++ src/pimocr.py
@@ -0,0 +1,103 @@
+"""OCR wrapper module
+
+This module defines various classes to run OCR functionnalities from several
+libraries or online tools.
+"""
+import pyocr
+
+
+class BaseOCR(object):
+    """Abstract base class for OCR modules
+
+    This class defines the basic model of OCR subclasses and should not be
+    instanciated.
+    """
+    def __init__(self):
+        pass
+
+    def set_file(self):
+        pass
+
+    def show(self, ax):
+        pass
+
+    def get_results(self, *args, **kwargs):
+        pass
+
+    def count_results(self):
+        pass
+
+
+class PyorcWrappedOCR(BaseOCR):
+    """Abstract class for pyocr wrapped tools
+
+    This class defines some functions for OCR tools based on pyocr library, and
+    should not be instanciated.
+    """
+    def __init__(self, tool_name=None):
+        """Constructor method for pyocr wrapped tools.
+
+        tool_name arg should always be provided, and from the following list
+        (see pyocr documentation): 'Tesseract (sh)', 'Tesseract (C-API)' or
+        'Cuneiform (sh)'.
+        """
+        tool_list = map(lambda x: x.get_name(), pyocr.get_available_tools())
+        if tool_name not in tool_list:
+            raise ValueError(f"Tool {tool_name} not installed on current env.")
+        self.tool_name = tool_name
+        self.wrapper_type = 'pyocr'
+
+    def set_file(self):
+        pass
+
+    def show(self, ax):
+        pass
+
+    def get_results(self, *args, **kwargs):
+        pass
+
+    def count_results(self):
+        pass
+
+
+class TextOCR(BaseOCR):
+    """Abstract class for text only OCR functionnalities
+
+    This class describes the text only OCR functionnalities, and
+    should not be instanciated.
+    """
+    def __init__(self):
+        pass
+
+    def set_file(self):
+        pass
+
+    def show(self, ax):
+        pass
+
+    def get_results(self, *args, **kwargs):
+        pass
+
+    def count_results(self):
+        pass
+
+
+class PyocrTextOCR(PyorcWrappedOCR, TextOCR):
+    """Class that instantiate a text only pyocr wrapped tool
+
+    This class instanciates the pyocr raw text functionnality.
+    """
+    def __init__(self):
+        pass
+
+    def set_file(self):
+        pass
+
+    def show(self, ax):
+        pass
+
+    def get_results(self, *args, **kwargs):
+        pass
+
+    def count_results(self):
+        pass
