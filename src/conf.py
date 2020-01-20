fda93de33f4e1ac3bcb90e809618208a829bcc35
MASSE Pierre <pit.masse@gmail.com>
2020-01-20 14:26:14 +0100

    to push conf.py

---
+++ src/conf.py
@@ -0,0 +1,33 @@
+"""Configuration Module
+
+This module provides a class that enables to fetch configuration stored in the
+cfg folder and make it available to other python modules.
+"""
+
+import yaml
+import os
+
+
+class Config:
+
+    def __init__(self, env):
+        if env not in {'dev', 'int', 'rec', 'qat', 'prd'}:
+            raise ValueError(f'Specified env : {env} not expected')
+        self.path = os.path.join(os.path.dirname(__file__),
+                                 '../cfg/config.yaml')
+        stream = open(self.path, 'r')
+        data = yaml.safe_load(stream)
+        config_keys = data['cross_env'].keys()
+        for k in config_keys:
+            setattr(self, k, data['cross_env'][k])
+        config_keys = data[env].keys()
+        for k in config_keys:
+            setattr(self, k, data[env][k])
+
+
+"""
+config = Config()
+And then any file that wants to use it:
+
+from lib.project.Config import config
+"""
