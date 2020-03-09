71142da25bec4bc1c41b8ae7b18039bc64362485
MASSE Pierre <pit.masse@gmail.com>
2020-03-09 20:53:08 +0100

    enriched pimpdf module with pathos usage and new static methods

--- src/pimpdf.py
+++ src/pimpdf.py
@@ -3,15 +3,18 @@
 This module aims to enable to fetch data from PIM system, into local folders.
 """
 
-from io import StringIO
+import pandas as pd
+from multiprocessing import cpu_count
+from pathos.multiprocessing import ProcessPool as Pool
+from io import StringIO, BytesIO
+from functools import partial
+
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
-import pandas as pd
-from multiprocessing import Pool, cpu_count
 
 
 class PDFDecoder(object):
@@ -21,6 +24,8 @@ class PDFDecoder(object):
     @staticmethod
     def content_to_text(content):
         """Decodes the binary passed as argument
+
+        content arg must be a Bytesio object.
         """
         output_string = StringIO()
         parser = PDFParser(content)
@@ -34,47 +39,102 @@ class PDFDecoder(object):
         return(output_string.getvalue())
 
     @staticmethod
-    def path_to_text(path):
+    def path_to_text(path, missing_file='raise'):
         """Decodes file at local path in the form of a long string
         """
+        if missing_file not in {'raise', 'ignore'}:
+            raise ValueError(f'Unexpected value for missing_file parameter. '
+                             f'Got {missing_file} but only \'raise\' or '
+                             f'\'ignore\' are expected.')
         try:
             with open(path, mode='rb') as content:
                 return(PDFDecoder.content_to_text(content))
+        except FileNotFoundError:
+            if missing_file == 'raise':
+                raise
+            if missing_file == 'ignore':
+                print(f'File not found at path: {path}')
+                return('')
         except Exception as e:
             print(e)
-            print(f'Could not read file at path: {path}')
+            print(f'An error happened at path: {path}')
             return('')
 
     @staticmethod
-    def path_to_blocks(path, split_func=lambda x: x.split('\n\n')):
+    def text_to_blocks(text, split_func=lambda x: x.split('\n\n')):
+        """Splits text passed as an argument with splitter function
+
+        split_func should be defined as to return a list like object.
+        """
+        return(split_func(text))
+
+    @staticmethod
+    def text_to_blocks_series(text, index=None,
+                              split_func=lambda x: x.split('\n\n'),
+                              ):
+        """Splits text passed as an argument (using splitter func) to a Series
+
+        The index arg is passed as provided to pd.Series constructor, or if
+        it is a scalar it is broadcasted as a constant.
+        """
+        blocks_list = PDFDecoder.text_to_blocks(text,
+                                                split_func=split_func,
+                                                )
+        try:
+            return(pd.Series(blocks_list, index=index))
+        except TypeError:
+            index = [index] * len(blocks_list)
+            return(pd.Series(blocks_list, index=index))
+
+    @staticmethod
+    def path_to_blocks(path, split_func=lambda x: x.split('\n\n'),
+                       missing_file='raise'):
         """Decodes file at local path in the form of a list of blocks
 
         Blocks are part of the original string separated by at least 2
         carriage returns (i.e. with at least a single blank line between them)
         """
-        return(split_func(PDFDecoder.path_to_text(path)))
+        text = PDFDecoder.path_to_text(path, missing_file=missing_file)
+        return(PDFDecoder.text_to_blocks(text, split_func=split_func))
 
     @staticmethod
-    def paths_to_blocks(path_series):
+    def path_to_blocks_series(path,
+                              index=None,
+                              split_func=lambda x: x.split('\n\n'),
+                              missing_file='raise'):
+        """Decodes file at local path in the form a pd Series of blocks
+        """
+        text = PDFDecoder.path_to_text(path, missing_file=missing_file)
+        return(PDFDecoder.text_to_blocks_series(text,
+                                                split_func=split_func,
+                                                index=index))
+
+    @staticmethod
+    def paths_to_blocks(path_series, split_func=lambda x: x.split('\n\n'),
+                        missing_file='raise'):
         """Decodes files for each path in path list as a blocks Dataseries
 
-        Blocks are part of the original string separated by at least 2
-        carriage returns (i.e. with at least a single blank line between them)
-        The path list must be a pandas dataseries ; the return is another
-        dataseries, with the same indexes as the initial series.
+        Blocks are part of the original string after the splitting function
+        has been applied.
+        The input must be a pandas dataseries of paths.
+        The output is another pd Series, with the same indexes as the initial
+        series (broadcasted to match the block count for each path)
         """
         ds_list = []
         for uid, path in path_series.items():
-            try:
-                blocks = PDFDecoder.path_to_blocks(path)
-                index = [uid] * len(blocks)
-                ds_list.append(pd.Series(blocks, index=index))
-            except FileNotFoundError:
-                pass
+            ds = (PDFDecoder
+                  .path_to_blocks_series(path,
+                                         split_func=split_func,
+                                         index=uid,
+                                         missing_file=missing_file))
+            ds_list.append(ds)
         return(pd.concat(ds_list, axis=0))
 
     @staticmethod
-    def threaded_paths_to_blocks(path_series, processes=None):
+    def threaded_paths_to_blocks(path_series, processes=None,
+                                 split_func=lambda x: x.split('\n\n'),
+                                 missing_file='raise',
+                                 ):
         """Threaded version of paths_to_blocks method
 
         It takes as input a series which index is the uid of the products,
@@ -82,24 +142,54 @@ class PDFDecoder(object):
         processes argument is the number of processes to launch. If omitted,
         it defaults to the number of cpu cores on the machine.
         """
+        processer = partial(PDFDecoder.path_to_blocks_series,
+                            split_func=split_func, missing_file=missing_file)
         processes = processes if processes else cpu_count()
         print(f'Launching {processes} processes.')
-        with Pool(processes=processes) as pool:
-            ds_list = pool.starmap(PDFDecoder.single_path_to_blocks,
-                                   list(zip(path_series.index, path_series)))
+        with Pool(nodes=processes) as pool:
+            ds_list = pool.map(processer,
+                               path_series, path_series.index)
         return(pd.concat(ds_list, axis=0))
 
     @staticmethod
-    def single_path_to_blocks(uid, path):
-        """Returns a series of text blocks in the document with path `path`
+    def threaded_contents_to_text(content_series, processes=None,
+                                  ):
+        """Threaded version of content_to_text method
 
-        This method is used by threaded_path_to_blocks to enable
-        multiprocessing.
-        It takes as arguments an uid of a product (which is given back as the
-        return series repeated index) and returns a series whose values are
-        the identified text blocks in the document at path `path`
+        It takes as input a series which index is the uid of the products,
+        and the values are the content (in the form of bytes) of the
+        documents.
+        processes argument is the number of processes to launch. If omitted,
+        it defaults to the number of cpu cores on the machine.
         """
-        values = PDFDecoder.path_to_blocks(path)
-        index = [uid] * len(values)
-        ds = pd.Series(values, index=index)
+        processer = PDFDecoder.content_to_text
+        processes = processes if processes else cpu_count()
+        print(f'Launching {processes} processes.')
+        in_ds = content_series.apply(BytesIO)
+        with Pool(nodes=processes) as pool:
+            tuples = (list(in_ds.index),
+                      pool.map(processer, in_ds))
+        ds = pd.Series(tuples[1], index=tuples[0])
+        return(ds)
+
+    @staticmethod
+    def threaded_texts_to_blocks(text_series, processes=None,
+                                 split_func=lambda x: x.split('\n\n'),
+                                 ):
+        """Threaded version of text_to_blocks_series method
+
+        It takes as input a series which index is the uid of the products,
+        and the values are the content (in the form of bytes) of the
+        documents..
+        processes argument is the number of processes to launch. If omitted,
+        it defaults to the number of cpu cores on the machine.
+        """
+        processer = partial(PDFDecoder.text_to_blocks_series,
+                            split_func=split_func)
+        processes = processes if processes else cpu_count()
+        print(f'Launching {processes} processes.')
+        ds_list = []
+        with Pool(nodes=processes) as pool:
+            ds_list = pool.map(processer, text_series, text_series.index)
+        ds = pd.concat(ds_list, axis=0)
         return(ds)
--- test/test_pimpdf.py
+++ test/test_pimpdf.py
@@ -13,11 +13,65 @@ from src.pimpdf import PDFDecoder
 
 @pytest.fixture
 def test_data_001():
-    test_data = namedtuple('Test_data', ['pdf', 'txt', 'blocks'])
+    test_data = namedtuple('Test_data', ['pdf', 'txt', 'blocks', 'uid'])
     pdf_path = Path(__file__).parent / 'test_data' / '001_formatted_pdf.pdf'
     txt_path = Path(__file__).parent / 'test_data' / '001_text.txt'
     block_path = Path(__file__).parent / 'test_data' / '001_blocks.csv'
-    return(test_data(pdf_path, txt_path, block_path))
+    uid = '001'
+    return(test_data(pdf_path, txt_path, block_path, uid))
+
+
+@pytest.fixture
+def test_data_002():
+    test_data = namedtuple('Test_data', ['pdf', 'txt', 'blocks', 'uid'])
+    pdf_path = Path(__file__).parent / 'test_data' / '002_formatted_pdf.pdf'
+    txt_path = Path(__file__).parent / 'test_data' / '002_text.txt'
+    block_path = Path(__file__).parent / 'test_data' / '002_blocks.csv'
+    uid = '002'
+    return(test_data(pdf_path, txt_path, block_path, uid))
+
+
+@pytest.fixture
+def test_data_no_file():
+    test_data = namedtuple('Test_data', ['pdf', 'txt', 'blocks', 'uid'])
+    pdf_path = Path(__file__).parent / 'test_data' / 'NO_SUCH_FILE.pdf'
+    txt_path = Path(__file__).parent / 'test_data' / 'NO_SUCH_FILE.txt'
+    block_path = Path(__file__).parent / 'test_data' / 'NO_SUCH_FILE.csv'
+    uid = '003'
+    return(test_data(pdf_path, txt_path, block_path, uid))
+
+
+@pytest.fixture
+def test_path_series(test_data_001, test_data_002):
+    idx = pd.Index([test_data_001.uid, test_data_002.uid], name='uid')
+    data = [test_data_001.pdf, test_data_002.pdf]
+    return(pd.DataFrame(data, index=idx, columns=['path']))
+
+
+@pytest.fixture
+def make_target_series():
+    def transform_target(test_data):
+        ds = pd.read_csv(test_data.blocks,
+                         encoding='utf-8-sig',
+                         sep=';',
+                         header=None,
+                         names=['blocks'],
+                         squeeze=True,
+                         )
+        idx = pd.DataFrame([test_data.uid] * len(ds), columns=['idx'])
+        ds = pd.concat([ds, idx], axis=1).set_index('idx')['blocks']
+        form_feed_ds = pd.Series(['\x0c'], name='blocks',
+                                 index=[test_data.uid])
+        ds = pd.concat([ds, form_feed_ds], axis=0)
+        return(ds)
+    return(transform_target)
+
+
+@pytest.fixture
+def splitter_func():
+    def splitter(x):
+        return(re.split(r'\n *\n', x))
+    return(splitter)
 
 
 class TestPDFDecoder(object):
@@ -26,25 +80,123 @@ class TestPDFDecoder(object):
         target = Path(test_data_001.txt).read_text()
         target = target + '\x0c'
         assert PDFDecoder.path_to_text(test_data_001.pdf) == target
-        print(target)
-        print(PDFDecoder.path_to_text(test_data_001.pdf))
+
+    def test_path_to_text_2(self, test_data_002):
+        target = Path(test_data_002.txt).read_text(encoding='utf-8-sig')
+        target = target + '\x0c'
+        assert PDFDecoder.path_to_text(test_data_002.pdf) == target
 
     def test_path_to_text_no_file(self):
-        assert PDFDecoder.path_to_text('incorect/path') == ''
+        assert PDFDecoder.path_to_text('incorect/path',
+                                       missing_file='ignore') == ''
+        with pytest.raises(FileNotFoundError):
+            PDFDecoder.path_to_text('incorect/path')
 
-    def test_path_to_blocks(self, test_data_001):
+    def test_path_to_text_corrupted(self):
+        path = Path(__file__).parent / 'test_data' / '003_corrupted_pdf.pdf'
+        assert PDFDecoder.path_to_text(path, missing_file='raise') == ''
+
+    def test_path_to_text_incorrect_params(self):
+        with pytest.raises(ValueError):
+            PDFDecoder.path_to_text('', missing_file='incorrect')
+
+    def test_path_to_blocks(self, test_data_001, splitter_func):
         target = pd.read_csv(test_data_001.blocks,
-                             encoding='latin-1',
+                             encoding='utf-8-sig',
                              sep=';',
                              header=None,
-                             names=['blocks'])
-        form_feed_df = pd.DataFrame(['\x0c'], columns=['blocks'])
-        target = target.append(form_feed_df, ignore_index=True)
-
-        def splitter(x):
-            return(re.split(r'\n *\n', x))
-
+                             names=['blocks'],
+                             squeeze=True,
+                             )
+        target = list(target)
+        target.append('\x0c')
         blocks = PDFDecoder.path_to_blocks(test_data_001.pdf,
-                                           split_func=splitter)
-        df_blocks = pd.DataFrame(blocks, columns=['blocks'])
-        assert target.equals(df_blocks)
+                                           split_func=splitter_func)
+        assert target == blocks
+
+    def test_path_to_blocks_2(self, test_data_002, splitter_func):
+        target = pd.read_csv(test_data_002.blocks,
+                             encoding='utf-8-sig',
+                             sep=';',
+                             header=None,
+                             names=['blocks'],
+                             squeeze=True,
+                             )
+        target = list(target)
+        target.append('\x0c')
+        blocks = PDFDecoder.path_to_blocks(test_data_002.pdf,
+                                           split_func=splitter_func)
+        assert target == blocks
+
+    def test_path_to_blocks_series(self, test_data_001, test_data_002,
+                                   make_target_series, splitter_func):
+        target = make_target_series(test_data_001)
+        blocks_series = (PDFDecoder
+                         .path_to_blocks_series(test_data_001.pdf,
+                                                split_func=splitter_func,
+                                                index=test_data_001.uid)
+                         )
+        assert target.equals(blocks_series)
+        target = make_target_series(test_data_002)
+        blocks_series = (PDFDecoder
+                         .path_to_blocks_series(test_data_002.pdf,
+                                                split_func=splitter_func,
+                                                index=test_data_002.uid)
+                         )
+        assert target.equals(blocks_series)
+
+    def test_paths_to_blocks(self, test_data_001, test_data_002,
+                             make_target_series, splitter_func):
+        target_ds = pd.concat([make_target_series(test_data_001),
+                               make_target_series(test_data_002)],
+                              axis=0)
+        input = pd.Series([test_data_001.pdf, test_data_002.pdf],
+                          index=[test_data_001.uid, test_data_002.uid])
+        output = PDFDecoder.paths_to_blocks(input, split_func=splitter_func)
+        assert output.equals(target_ds)
+
+    def test_paths_to_blocks_with_missing(self, test_data_001, test_data_002,
+                                          make_target_series, splitter_func,
+                                          test_data_no_file):
+        no_file_ds = pd.Series([''], index=[test_data_no_file.uid])
+        target_ds = pd.concat([make_target_series(test_data_001),
+                               make_target_series(test_data_002),
+                               no_file_ds],
+                              axis=0)
+        input = pd.Series([test_data_001.pdf,
+                           test_data_002.pdf,
+                           test_data_no_file.pdf,
+                           ],
+                          index=[test_data_001.uid,
+                                 test_data_002.uid,
+                                 test_data_no_file.uid,
+                                 ])
+        output = PDFDecoder.paths_to_blocks(input,
+                                            split_func=splitter_func,
+                                            missing_file='ignore')
+        assert output.equals(target_ds)
+        output = PDFDecoder.threaded_paths_to_blocks(input,
+                                                     split_func=splitter_func,
+                                                     missing_file='ignore')
+        assert output.equals(target_ds)
+
+    def test_content_to_text_to_blocks(self, test_data_001, test_data_002,
+                                       make_target_series, splitter_func,
+                                       ):
+        # 1 je construit la Series avec les contents
+        # 2 j'applique content to text
+        # 3 j'applique text to blocks
+        target_ds = pd.concat([make_target_series(test_data_001),
+                               make_target_series(test_data_002)],
+                              axis=0)
+        content_list = []
+        for test_data in [test_data_001, test_data_002]:
+            with open(test_data.pdf, mode='rb') as content:
+                content_list.append(content.read())
+        content_ds = pd.Series(content_list, index=[test_data_001.uid,
+                                                    test_data_002.uid])
+        text_ds = PDFDecoder.threaded_contents_to_text(content_ds)
+        blocks_ds = (PDFDecoder
+                     .threaded_texts_to_blocks(text_ds,
+                                               split_func=splitter_func))
+        assert blocks_ds.equals(target_ds)
