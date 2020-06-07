1cc2bafb8ecf3de0e90ce84a1d8479e8c4a3f412
MASSE Pierre <pit.masse@gmail.com>
2020-06-07 22:12:43 +0200

    more work

--- build_notebooks_pdf.bat
+++ build_notebooks_pdf.bat
@@ -1,18 +1,19 @@
 echo off
 cd C:\Users\pmasse\Pyprojects\PIM-Recognizer\report\notebooks
-echo Lancement 1/7
+echo Lancement 1/8
 jupyter nbconvert --to pdf "Analyse donnees du PIM.ipynb"
-echo Lancement 2/7
+echo Lancement 2/8
 jupyter nbconvert --to pdf "Analyse quantitative.ipynb"
-echo Lancement 3/7
+echo Lancement 3/8
 jupyter nbconvert --to pdf ground_truth_constitution.ipynb
-echo Lancement 4/7
+echo Lancement 4/8
 jupyter nbconvert --to pdf gt_based_model.ipynb
-echo Lancement 5/7
+echo Lancement 5/8
 jupyter nbconvert --to pdf open_model.ipynb
-echo Lancement 6/7
+echo Lancement 6/8
 jupyter nbconvert --to pdf Performance_measurement.ipynb
-echo Done
-echo Lancement 7/7
+echo Lancement 7/8
 jupyter nbconvert --to pdf model_tuning.ipynb
+echo Lancement 8/8
+jupyter nbconvert --to pdf Text_analysis.ipynb
 echo Done
